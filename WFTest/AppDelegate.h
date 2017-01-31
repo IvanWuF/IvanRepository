c0ae5a227846be353926ba60f027d658e20854d9
ivan <ivan@ivandeMac.local>
2017-01-30 19:48:26 -0800

    Initial Commit

---
+++ WFTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WFTest
+//
+//  Created by ivan on 2017/1/30.
+//  Copyright © 2017年 ivan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WFTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WFTest
+//
+//  Created by ivan on 2017/1/30.
+//  Copyright © 2017年 ivan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
